604c2250ccc4820aa6d7d28268868758c5ea539e
Meiqi Wang <meiqw@brandeis.edu>
2020-07-28 21:11:43 -0400

    third commit

--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,15 @@
+"""
+This python file reads the csv file under input directory and outputs related data analysis into output directory
+"""
+
 import csv
 from collections import defaultdict
 import sys
 
+__author__ = 'Meiqi Wang'
+
+#read_csv function reads the csv file and convert the data into a dictionary, the dictionary stores products
+#, year, company and the complaints each company received
 def read_csv(filename):
     pro_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
     with open(filename, mode='r') as csv_file:
@@ -14,10 +22,13 @@ def read_csv(filename):
             product = row[header_dict['product']].strip().lower()
             year = row[header_dict['date received']].split("-")[0]
             company = row[header_dict['company']].strip().upper()
+            #count the number of complaints each company received each year for each product
             pro_dict[product][year][company] += 1
-    #print(pro_dict)
+
     return pro_dict
 
+#write_csv function loops through the dictionary returned from read_csv function, does some calculation
+#and writes the fields into the output csv file
 def write_csv(dict, filename):
     with open(filename, mode='w') as f:
         f_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
@@ -25,8 +36,11 @@ def write_csv(dict, filename):
         #f_writer.writerow(fieldnames)
         for pro, v in sorted(dict.items()):
             for year, comps in sorted(v.items()):
+                #add up the total number of complaints of the product in the year
                 total = sum(comps.values())
+                #get the number of companies that received the complaint
                 comp_total = len(comps)
+                #calculate the highest percentage of total complaints filed against one company
                 p = round(float(max(comps.values())) / total, 2)
                 percent = int(p * 100)
                 f_writer.writerow([pro, year, total, comp_total, percent])
@@ -37,5 +51,3 @@ if __name__ == '__main__':
     output = sys.argv[2]
     dict = read_csv(input)
     write_csv(dict, output)
-    #dict = read_csv('../insight_testsuite/test_1/input/complaints.csv')
-    #write_csv(dict, '../insight_testsuite/test_1/output/report.csv')
